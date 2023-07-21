487bb2dd3e06954acfb482a0d9e592badf23cb9b
Eric Ducasse <eric.ducasse@u-bordeaux.fr>
2023-07-21 10:47:35 +0200

    Version 1.64 - 2023 July 20 - 3d anisotropic plate example added

--- Documentation/Installation/B_TraFiC_Installation.md
+++ Documentation/Installation/B_TraFiC_Installation.md
@@ -6,5 +6,6 @@
    > <tt><b>(trafic_env)...> cd <i><TraFiC_root_path></i> </b></tt>  
    > <tt><b>(trafic_env)...> python TraFiC_installer.py </b></tt>   
 1. To Launch the GUI (Incomplete software under development):   
+   > <tt><b>(trafic_env)...> cd TraFiC</b></tt>  
    > <tt><b>(trafic_env)...> python TraFiC_GUI_Launcher.py </b></tt>    
-1. Basic examples without GUI are in the directory <tt><b>Examples_without_GUI</b></tt>.   
+2. Basic examples without GUI are in the directory <tt><b>Examples_without_GUI</b></tt>.   
--- TraFiC_installer.py
+++ TraFiC_installer.py
@@ -1,16 +1,16 @@
-# Version 1.63 - 2023, July 18
+# Version 1.64 - 2023, July 20
 # Copyright (Eric Ducasse 2020)
 # Licensed under the EUPL-1.2 or later
 # Institution:  I2M / Arts & Metiers ParisTech
 # Program name: TraFiC (Transient Field Computation)
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-from Basic_Tools.TraFiC_utilities import now 
+from TraFiC.Basic_Tools.TraFiC_utilities import now 
 import os
-root = os.path.dirname(__file__).replace("\\","/")
+root = os.path.dirname(__file__).replace("\\","/")+"/TraFiC"
 print(f"TraFiC_installer :: root : '{root}'")
 with open("./TraFiC_init_template.txt","r",encoding="utf-8") as f :
     incomplete_code = f.read()
 complete_code = incomplete_code.format(root, now(True))
-with open("./TraFiC_init.py","w",encoding="utf-8") as f :
+with open("./TraFiC/TraFiC_init.py","w",encoding="utf-8") as f :
     f.write(complete_code)
 print("TraFiC_installer :: TraFiC_init.py created")
